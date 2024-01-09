6ce072bc2084b11afdbf11dea2dff6b42b9316d2
Echo <echo@cortex.foundation>
2024-01-09 08:03:55 +0000

    fix handling subtensor errors

--- template/base/validator.py
+++ template/base/validator.py
@@ -133,30 +133,29 @@ class BaseValidatorNeuron(BaseNeuron):
             while True:
                 bt.logging.info(f"step({self.step}) block({self.block})")
 
-                # Run multiple forwards concurrently.
-                self.loop.run_until_complete(self.concurrent_forward())
+                try:
+                    # Run multiple forwards concurrently.
+                    self.loop.run_until_complete(self.concurrent_forward())
 
-                # Check if we should exit.
-                if self.should_exit:
-                    break
+                    # Check if we should exit.
+                    if self.should_exit:
+                        break
 
-                # Sync metagraph and potentially set weights.
-                self.sync()
+                    # Sync metagraph and potentially set weights.
+                    self.sync()
 
-                self.step += 1
+                    self.step += 1
 
-        # If someone intentionally stops the validator, it'll safely terminate operations.
+                except Exception as err:
+                    bt.logging.error("Error during validation", str(err))
+                    bt.logging.debug(print_exception(type(err), err, err.__traceback__))
+                    # Continue to the next iteration of the loop
+                    continue
+
         except KeyboardInterrupt:
             self.axon.stop()
             bt.logging.success("Validator killed by keyboard interrupt.")
             exit()
-
-        # In case of unforeseen errors, the validator will log the error and continue operations.
-        except Exception as err:
-            bt.logging.error("Error during validation", str(err))
-            bt.logging.debug(
-                print_exception(type(err), err, err.__traceback__)
-            )
 
     def run_in_background_thread(self):
         """
