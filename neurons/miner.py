846787be283cc52fcf46f72e748bff4cdde10365
Echo <echo@cortex.foundation>
2024-01-23 09:10:06 +0000

    :art: Add neurons folder for auto update

---
+++ neurons/miner.py
@@ -0,0 +1,20 @@
+import sys
+import subprocess
+import os
+path = os.path.dirname(os.path.realpath(__file__))
+# Predefined path of the script to run
+SCRIPT_PATH = f"{path}/../src/flavia/neurons/miner/miner.py"
+
+def run_script(args):
+    # Build the complete command with arguments
+    command = [sys.executable, SCRIPT_PATH] + args
+
+    # Execute the script
+    subprocess.run(command)
+
+if __name__ == "__main__":
+    # Get arguments passed to this script (excluding the script name itself)
+    args = sys.argv[1:]
+
+    # Run the script with the arguments
+    run_script(args)
